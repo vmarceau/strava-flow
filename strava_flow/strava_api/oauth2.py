5ae67d452c1f7d929ded3dac4a97ed8ef9402f14
Vincent Marceau <vmarceau@gmail.com>
2019-02-10 20:10:49 -0500

    Added OAuth2AuthenticationClient

--- strava_flow/strava_api/oauth2.py
+++ strava_flow/strava_api/oauth2.py
@@ -1,19 +1,19 @@
+import webbrowser
 from typing import List, Dict, Any, Tuple
-from urllib.parse import urlparse, parse_qs
+from requests_oauthlib import OAuth2Session  # type: ignore
 from http.server import BaseHTTPRequestHandler, HTTPServer, HTTPStatus  # type: ignore
 
 
 class OAuth2RedirectServer(HTTPServer):
     def __init__(self, server_address: Tuple[str, int], request_handler: type) -> None:
         super().__init__(server_address, request_handler)
-        self.query_params: Dict[str, Any] = {}
+        self.response_url = ''
 
 
 class OAuth2RedirectHandler(BaseHTTPRequestHandler):
     def do_GET(self) -> None:
         self._status_ok()
-        query = self._parse_query_params()
-        self.server.query_params = query  # type: ignore
+        self.server.response_url = self.path  # type: ignore
         self._write_authentication_status()
 
     def log_message(self, format: str, *args: List[Any]) -> None:
@@ -24,13 +24,61 @@ class OAuth2RedirectHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def _parse_query_params(self) -> Dict[str, Any]:
-        parts = urlparse(self.path)
-        urlencoded_params = parse_qs(parts.query)
-        params = {key: value[0] for key, value in urlencoded_params.items()}
-        return params
-
     def _write_authentication_status(self) -> None:
         self.wfile.write(b'<html><head><title>Authentication Status</title></head>')
         self.wfile.write(b'<body><p>Authentication has completed successfully.</p>')
         self.wfile.write(b'</body></html>')
+
+
+class OAuth2AuthenticationClient:
+    _REDIRECT_SERVER_HOST = ''
+    _REDIRECT_SERVER_PORT = 0
+
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        scopes: List[str],
+        user_agent: str,
+        auth_uri: str,
+        token_uri: str,
+        revoke_uri: str,
+    ) -> None:
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._scopes = scopes
+        self._user_agent = user_agent
+        self._auth_uri = auth_uri
+        self._token_uri = token_uri
+        self._revoke_uri = revoke_uri
+        self._redirect_uri = f'http://{self._REDIRECT_SERVER_HOST}:{self._REDIRECT_SERVER_PORT}/'
+
+    def authenticate(self) -> Dict[str, Any]:
+        httpd = OAuth2RedirectServer((self._REDIRECT_SERVER_HOST, self._REDIRECT_SERVER_PORT), OAuth2RedirectHandler)
+        authorization_url, state = self._get_authorization_url()
+
+        webbrowser.open(authorization_url, new=1, autoraise=True)
+        httpd.handle_request()
+        response = httpd.response_url
+
+        token = self._fetch_token(response, state)
+        return token
+
+    def refresh(self, token: Dict[str, Any]) -> Dict[str, Any]:
+        strava = OAuth2Session(self._client_id, token=token)
+        new_token: Dict[str, Any] = strava.refresh_token(
+            self._token_uri, client_id=self._client_id, client_secret=self._client_secret
+        )
+        return new_token
+
+    def _get_authorization_url(self) -> Tuple[str, str]:
+        strava = OAuth2Session(self._client_id, scope=self._scopes, redirect_uri=self._redirect_uri)
+        authorization_url, state = strava.authorization_url(self._auth_uri)
+        return authorization_url, state
+
+    def _fetch_token(self, response: str, state: str) -> Dict[str, Any]:
+        strava = OAuth2Session(self._client_id, redirect_uri=self._redirect_uri, state=state)
+        token: Dict[str, Any] = strava.fetch_token(
+            self._token_uri, client_secret=self._client_secret, authorization_response=response
+        )
+        return token
